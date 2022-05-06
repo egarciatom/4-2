7ffed73fd84de75960acb207c28578f22e522b4d
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-05-06 14:19:57 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("")
+})
+radio.onReceivedString(function (receivedString) {
+    if ("pozik" == receivedString) {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("")
+})
+radio.setGroup(30)
